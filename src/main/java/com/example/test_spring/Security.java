f4ce900cd0058d0b838cc3309d13b53d7a04a155
Alibek Abilkassymov <al16ek199@gmail.com>
2024-07-29 11:42:55 +0500

    init

---
+++ src/main/java/com/example/test_spring/Security.java
@@ -0,0 +1,28 @@
+package com.example.test_spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Security {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests(authorization -> authorization
+                        .requestMatchers("/").permitAll()
+                );
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/example/test_spring/TestController.java
@@ -0,0 +1,16 @@
+package com.example.test_spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String test_resource() {
+        return "hello";
+    }
+}
